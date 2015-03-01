83d11df3c8733bb6ff8dbe42bced756f31081255
Mengtian Li <lmt822@hotmail.com>
2015-03-01 15:05:14 -0500

    Adding map

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,15 @@
-// Replace this with your code...
+/* Mengtian Li
+ * mli09
+ * 2/28/2015
+ * game.js
+ */
+function draw(){
+	canvas = document.getElementById('game_canvas');
+	ctx = canvas.getContext('2d');
+	var img = new Image();
+	img.onload = function{
+		ctx.drawImage(img,)
+	}
+}
+
+
