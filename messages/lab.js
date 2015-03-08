3b727b8257abc7c9be4bec484e646939e142ec7a
Mengtian Li <lmt822@hotmail.com>
2015-03-08 14:19:26 -0400

    innerhtml should work

--- messages/lab.js
+++ messages/lab.js
@@ -8,6 +8,7 @@ function parse(){
 	xhr.send();
 	function myCallBackFunc(){
 		data = JSON.parse(xhr.responseText);
-		document.getElementById("messages").innerHTML = data[0].content + data[0].username + <br> + data[1].content + data[1].username";
+		var newHTML = data[0].content + data[0].username + "<br>" + data[1].content + data[1].username;
+		document.getElementById("messages").innerHTML = newHTML;
 	}
 }
